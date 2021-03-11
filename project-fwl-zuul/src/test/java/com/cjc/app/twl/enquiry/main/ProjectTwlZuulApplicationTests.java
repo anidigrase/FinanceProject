7e66127f31e0cdbb69fe93a488c1fcc630dd7373
AESS <AESS@DESKTOP-RV85UHQ>
2021-03-11 13:10:11 +0530

    Added Zuul Server

---
+++ project-fwl-zuul/src/test/java/com/cjc/app/twl/enquiry/main/ProjectTwlZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc.app.twl.enquiry.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTwlZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
